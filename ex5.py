82d08a296aa23b7238c06f6c3a0a2287815dfac0
joaopavan-ort <joao.pavan@ort.org.br>
2026-05-27 14:52:05 -0300

    update 5

--- ex5.py
+++ ex5.py
@@ -2,7 +2,6 @@ notas = [7.5, 9.0, 5.2, 8.0, 6.5]
 
 listaOrdenada = sorted(notas)
 print(listaOrdenada)
-if 5.2 in notas:
-    check = True
+check = 'SIM' if 5.2 in notas else 'NÃO'
 print(check)
 print(notas.index(9.0))
